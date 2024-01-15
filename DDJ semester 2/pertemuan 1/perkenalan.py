2836b862410352ed19fc39998b73d05c74903bda
NabilAbdulFajar <148731857+NabilAbdulFajar@users.noreply.github.com>
2024-01-15 09:41:59 +0700

    Add files via upload

---
+++ DDJ semester 2/pertemuan 1/perkenalan.py
@@ -0,0 +1,8 @@
+Nama = "Nabil Abdul Fajar"
+Kelas = "X-A"
+Jurusan = "TKJ"
+
+print ("Halo perkenalkan nama saya", Nama)
+print (" saya kelas ", Kelas, "dari Jurusan", Jurusan)
+
+
---
+++ DDJ semester 2/pertemuan 1/variable.py
@@ -0,0 +1,22 @@
+x = 7
+y = 2
+z = "SMKS MEDIKACOM"
+P = False
+q = True
+
+print(x)
+print(y)
+print(z)
+print(P)
+print(q)
+
+print("cek tipe data")
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(P))
+print(type(q))
+
+
+
+
